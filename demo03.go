65c3c3a08931e4253fafe5956f43ae19ddbc95ab
simpleboy <2607027653@qq.com>
2023-05-06 16:22:40 +0800

    new learn

--- demo03.go
+++ demo03.go
@@ -5,3 +5,22 @@
  */
 
 package main
+
+import "github.com/gin-gonic/gin"
+
+func getPostval(c *gin.Context) {
+	username := c.PostForm("name")
+	passwd := c.DefaultPostForm("passwd", "xxxxxxxx")
+	c.String(200, "获取到的name = %s,paswd = %s ", username, passwd)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/demo03get", func(c *gin.Context) {
+		username := c.Query("name")
+		passwd := c.DefaultQuery("passwd", "xxxxxx") //第二个是默认值.
+		c.String(200, "this is getkey() name= %s  passwd = %s ", username, passwd)
+	})
+	router.POST("/demo03post", getPostval)
+	router.Run(":8000")
+}
